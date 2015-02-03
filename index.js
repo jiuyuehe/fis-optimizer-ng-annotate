5822589929f935cee265efb9359bfe1307779f0f
googleyufei <googleyufei@qq.com>
2015-02-03 15:12:12 +0800

    optimize: just process angular js files

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 var esprima = require('esprima'),
-  escodegen = require('escodegen'),
-  astral = require('astral')();
+    escodegen = require('escodegen'),
+    astral = require('astral')();
 
 require('astral-angular-annotate')(astral);
 
-module.exports = function(content, file, conf) {
-    var ast = esprima.parse(content, {
-      tolerant: true
-    });
+module.exports = function (content, file, conf) {
+    if (file.isJsLike && file.isAngular) {
+        var ast = esprima.parse(content, {
+            tolerant: true
+        });
 
-    astral.run(ast);
+        astral.run(ast);
 
-    return escodegen.generate(ast, {
-      format: {
-        indent: {
-          style: '  '
-        }
-      }
-    });
+        return escodegen.generate(ast, {
+            format: {
+                indent: {
+                    style: '  '
+                }
+            }
+        });
+    } else {
+        return content;
+    }
 };
